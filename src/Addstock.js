abac44558d09ad311c203a90d43c9a9ebb212fcc
the-sigma-bitcoin <yash.chaudhari@coindcx.com>
2022-08-03 21:49:40 +0530

    added forms in add-stock

--- src/Addstock.js
+++ src/Addstock.js
@@ -1,5 +1,45 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
+import { useState } from 'react';
+import ReactDOM from 'react-dom/client';
+
+function MyForm() {
+  const [inputs, setInputs] = useState({});
+
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setInputs(values => ({...values, [name]: value}))
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert(inputs);
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>Enter Stock name:
+      <input 
+        type="text" 
+        name="sName" 
+        value={inputs.sName || ""} 
+        onChange={handleChange}
+      />
+      </label>
+      <label>Enter Stock Price:
+        <input 
+          type="number" 
+          name="sPrice" 
+          value={inputs.sPrice || ""} 
+          onChange={handleChange}
+        />
+        </label>
+        <input type="submit" />
+    </form>
+  )
+}
+
 
 const PnlComponent = () => {
 
@@ -9,6 +49,7 @@ const PnlComponent = () => {
     return (
         <div>
             <h2>Add a stock</h2>
+            <MyForm />
 
         </div>
     );
